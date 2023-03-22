18670021691e29c1c9705c0383d3dc19b5f2c9e3
Rujun Chen <rujche@microsoft.com>
2023-03-22 16:51:04 +0800

    Create ReadWriteUtil.readUsersFromTxtFile.

--- src/main/java/com/azure/spring/example/file/processing/util/ReadWriteUtil.java
+++ src/main/java/com/azure/spring/example/file/processing/util/ReadWriteUtil.java
@@ -10,8 +10,12 @@ import org.apache.avro.specific.SpecificDatumWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReadWriteUtil {
     private static final Logger LOGGER = LoggerFactory.getLogger(ReadWriteUtil.class);
@@ -32,5 +36,31 @@ public class ReadWriteUtil {
             LOGGER.debug("Read user from avro file. user = {}, file = {}. ", user, file.getAbsolutePath());
             return user;
         }
+    }
+
+    public static List<User> readUsersFromTxtFile(File file) throws IOException {
+        List<User> result = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line = reader.readLine();
+            while (line != null) {
+                result.add(toUser(line));
+                line = reader.readLine();
+            }
+        }
+        return result;
+    }
+
+    public static User toUser(String string) {
+        LOGGER.debug("Convert string to user. string = '{}'. ", string);
+        String[] items = string.split(",");
+        if (items.length != 3) {
+            LOGGER.debug("Convert string to user failed. items.length = {}. ", items.length);
+            return null;
+        }
+        return User.newBuilder()
+                .setName(items[0])
+                .setFavoriteColor(items[1])
+                .setFavoriteNumber(Integer.valueOf(items[2]))
+                .build();
     }
 }
--- src/test/java/com/azure/spring/example/file/processing/util/ReadWriteUtilTest.java
+++ src/test/java/com/azure/spring/example/file/processing/util/ReadWriteUtilTest.java
@@ -5,22 +5,54 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.Objects;
 
+import static com.azure.spring.example.file.processing.util.ReadWriteUtil.readUserFromAvroFile;
+import static com.azure.spring.example.file.processing.util.ReadWriteUtil.readUsersFromTxtFile;
+import static com.azure.spring.example.file.processing.util.ReadWriteUtil.toUser;
+import static com.azure.spring.example.file.processing.util.ReadWriteUtil.writeUserToAvroFile;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class ReadWriteUtilTest {
 
+    private static final String USER_1_STRING = "User1,blue,1";
     private static final User USER_1 = User.newBuilder()
             .setName("User1")
             .setFavoriteColor("blue")
-            .setFavoriteNumber(3)
+            .setFavoriteNumber(1)
+            .build();
+    private static final User USER_2 = User.newBuilder()
+            .setName("User2")
+            .setFavoriteColor("red")
+            .setFavoriteNumber(2)
             .build();
 
     @Test
     public void oneUserWriteThenReadTest() throws IOException {
         File file = File.createTempFile("ReadWriteUtilTest-", ".txt");
-        ReadWriteUtil.writeUserToAvroFile(USER_1, file);
-        User user = ReadWriteUtil.readUserFromAvroFile(file);
+        writeUserToAvroFile(USER_1, file);
+        User user = readUserFromAvroFile(file);
         assertEquals(USER_1, user);
+    }
+
+    @Test
+    public void toUserTest() {
+        assertEquals(USER_1, toUser(USER_1_STRING));
+    }
+
+    @Test
+    public void readUsersFromTxtFileTest() throws IOException {
+        URL url = getClass().getClassLoader().getResource("userString.txt");
+        Objects.requireNonNull(url);
+        File file = new File(url.getFile());
+        List<User> users = readUsersFromTxtFile(file);
+        assertEquals(4, users.size());
+        assertEquals(USER_1, users.get(0));
+        assertNull(users.get(1));
+        assertEquals(USER_2, users.get(2));
+        assertNull(users.get(3));
     }
 }
