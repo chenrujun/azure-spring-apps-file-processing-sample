e0ef0fd9044d7d6a4922a249d90ec57a18d97173
Rujun Chen <rujche@microsoft.com>
2023-03-30 17:12:33 +0800

    Rename DemoApplication to FileProcessingApplication.

--- src/main/java/com/azure/spring/example/file/processing/FileProcessingApplication.java
+++ src/main/java/com/azure/spring/example/file/processing/FileProcessingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class FileProcessingApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class);
+        SpringApplication.run(FileProcessingApplication.class);
     }
 
 }
