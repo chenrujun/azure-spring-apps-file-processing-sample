019aac71642a3dafc543ccbee6e4f40cec0d63a3
Rujun Chen <rujche@microsoft.com>
2023-03-30 16:58:17 +0800

    Add feature: Move file after file handled.

--- src/main/java/com/azure/spring/example/file/processing/IntegrationConfiguration.java
+++ src/main/java/com/azure/spring/example/file/processing/IntegrationConfiguration.java
@@ -22,26 +22,30 @@ public class IntegrationConfiguration {
     private static final Logger LOGGER = LoggerFactory.getLogger(IntegrationConfiguration.class);
 
     private final String inputDirectory;
+    private final String outputDirectory;
     private final String eventHubName;
     private final EventHubsTemplate eventHubsTemplate;
 
     public IntegrationConfiguration(@Value("${input-directory}") String inputDirectory,
+                                    @Value("${output-directory}") String outputDirectory,
                                     @Value("${spring.cloud.azure.eventhubs.event-hub-name}") String eventHubName,
                                     EventHubsTemplate eventHubsTemplate) {
-        this.inputDirectory = inputDirectory;
+        this.inputDirectory = toAbsolutePath(inputDirectory);
+        this.outputDirectory = toAbsolutePath(outputDirectory);
         this.eventHubName = eventHubName;
         this.eventHubsTemplate = eventHubsTemplate;
-        LOGGER.info("inputDirectory = {}, eventHubName = {}.", inputDirectory, eventHubName);
+        LOGGER.info("inputDirectory = {}, outputDirectory = {}, eventHubName = {}.", inputDirectory, outputDirectory, eventHubName);
     }
 
     @Bean
+    @SuppressWarnings("unchecked")
     public IntegrationFlow fileReadingFlow() {
         return IntegrationFlow
                 .from(Files.inboundAdapter(new File(inputDirectory)).recursive(true).useWatchService(true),
                         e -> e.poller(Pollers.fixedDelay(0).advice(new ExitSystemReceiveMessageAdvice())))
                 .filter(FileMessageUtil::isTargetFile)
                 .transform(Files.toStringTransformer())
-                .transform(Message.class, FileMessageUtil::toTxtLine)
+                .transform(Message.class, message -> FileMessageUtil.toTxtLineThenMoveFile((Message<String>) message, inputDirectory, outputDirectory)) // TODO: Improvements: 1. Move file after send message to event hub. 2. Move file that isTargetFile() == false.
                 .split()
                 .transform(Message.class, FileMessageUtil::toAvroBytes)
                 .handle(defaultMessageHandler())
@@ -52,6 +56,12 @@ public class IntegrationConfiguration {
         DefaultMessageHandler handler = new DefaultMessageHandler(eventHubName, eventHubsTemplate);
         handler.setSendCallback(new OnFailureExitSystemCallback());
         return handler;
+    }
+
+    private String toAbsolutePath(String directory) {
+        String separator = File.separator;
+        String systemCompatibleDirectory = directory.replace("/", separator).replace("\\", separator);
+        return new File(systemCompatibleDirectory).getAbsolutePath();
     }
 
 }
--- src/main/java/com/azure/spring/example/file/processing/util/FileMessageUtil.java
+++ src/main/java/com/azure/spring/example/file/processing/util/FileMessageUtil.java
@@ -5,24 +5,56 @@ import org.slf4j.LoggerFactory;
 import org.springframework.messaging.Message;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import static java.util.regex.Matcher.quoteReplacement;
 
 public class FileMessageUtil {
 
     public static final Logger LOGGER = LoggerFactory.getLogger(FileMessageUtil.class);
     public static final String FILE_ORIGINAL_FILE = "file_originalFile";
 
-    public static List<TxtLine> toTxtLine(Message<String> message) {
+    public static List<TxtLine> toTxtLineThenMoveFile(Message<String> message, String inputDirectory, String outputDirectory) {
         String[] lines = message.getPayload().split("\\r?\\n");
         List<TxtLine> txtLines = new ArrayList<>();
         for (int i = 0; i < lines.length; i++) {
             txtLines.add(new TxtLine(i + 1, lines[i].trim()));
         }
         LOGGER.info("Split one file into lines. file = {}, lineNumber = {}.", getAbsolutePath(message), txtLines.size());
+        moveFile(message, inputDirectory, outputDirectory);
         return txtLines;
+    }
+
+    public static void moveFile(Message<String> message, String inputDirectory, String outputDirectory) {
+        File file = getOptionalFile(message).orElse(null);
+        if (file == null) {
+            LOGGER.warn("Move file failed because file is null.");
+            return;
+        }
+        String oldPath = getAbsolutePath(message);
+        String newPath = newPath(oldPath, inputDirectory, outputDirectory);
+        File newFile = new File(newPath);
+        try {
+            Files.createDirectories(newFile.getParentFile().getAbsoluteFile().toPath());
+        } catch (IOException e) {
+            LOGGER.warn("Move file failed because cannot create directory. oldPath = {}, newPath = {}.", oldPath, newPath, e);
+            return;
+        }
+        boolean moveSucceed = file.renameTo(newFile);
+        if (moveSucceed) {
+            LOGGER.info("Move file succeed. oldPath = {}, newPath = {}.", oldPath, newPath);
+        } else {
+            LOGGER.warn("Move file failed. oldPath = {}, newPath = {}.", oldPath, newPath);
+        }
+    }
+
+    private static String newPath(String currentPath, String inputDirectory, String outputDirectory) {
+        return currentPath.replaceFirst(quoteReplacement(inputDirectory), quoteReplacement(outputDirectory));
     }
 
 
@@ -42,10 +74,14 @@ public class FileMessageUtil {
         return AvroUtil.toAvroBytes(absolutePath, lineNumber, content);
     }
 
-    public static String getAbsolutePath(Message<?> message) {
+    public static Optional<File> getOptionalFile(Message<?> message) {
         return Optional.of(message)
                 .map(Message::getHeaders)
-                .map(headers -> (File) headers.get(FILE_ORIGINAL_FILE))
+                .map(headers -> (File) headers.get(FILE_ORIGINAL_FILE));
+    }
+
+    public static String getAbsolutePath(Message<?> message) {
+        return getOptionalFile(message)
                 .map(File::getAbsolutePath)
                 .orElse(null);
     }
